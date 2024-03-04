77ab2509485e40a112f93ffb9348f1fcf94ad07a
souzafe13 <souzafe13@gmail.com>
2024-03-04 17:03:49 -0300

    Node

---
+++ primeirospassos/app.js
@@ -0,0 +1,9 @@
+var nome = "Fernanda"
+
+var sobrenome = "Souza";
+
+var caracteristica = "Linda e Perfeita";
+
+var fato = "diva";
+
+console.log(nome + " " + sobrenome + " " + caracteristica + " " + fato);
