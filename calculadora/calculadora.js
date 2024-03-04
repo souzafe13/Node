1baa9fd31dacea33c034ff1518aa9f5803494ed4
souzafe13 <souzafe13@gmail.com>
2024-03-04 17:24:41 -0300

    Node

---
+++ calculadora/calculadora.js
@@ -0,0 +1,10 @@
+var somaFunc = require("./soma");
+var subtraiFunc = require("./subtracao");
+var multiplicaFunc = require("./multiplicacao");
+var divideFunc = require("./divisao");
+
+console.log(somaFunc(1,2));
+console.log(subtraiFunc(2,2));
+console.log(multiplicaFunc(2,2));
+console.log(divideFunc(4,2));
+
